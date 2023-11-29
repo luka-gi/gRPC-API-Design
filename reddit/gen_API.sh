daae1e79a946bac08e97f2b6002e43eaef49f150
luka-gi <lupblanch@hotmail.com>
2023-11-28 22:03:30 -0500

    update paths to separate services

--- reddit/gen_API.sh
+++ reddit/gen_API.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc -I. --python_out=. --pyi_out=. --grpc_python_out=. ./reddit.proto
+python -m grpc_tools.protoc -I. --python_out=client --pyi_out=client --grpc_python_out=client ./reddit.proto
+python -m grpc_tools.protoc -I. --python_out=server --pyi_out=server --grpc_python_out=server ./reddit.proto
